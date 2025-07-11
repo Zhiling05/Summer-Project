3e4324ce162a9199a6a7ce8c0d150cd02ce80894
Zhiling05 <liuzhilingwh@gmail.com>
2025-07-11 15:23:03 +0100

    Delete the navigation folder

--- docs/src/pages/navigation/index.tsx
+++
@@ -1,4 +0,0 @@
-// src/pages/navigation/index.ts
-export { default as SettingsPage } from './SettingsPage';
-export { default as AboutPage }    from './AboutPage';
-export { default as ContactPage }  from './ContactPage';
