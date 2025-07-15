44526cfd10fab7a7592cdf15649dfac13d594739
Charlyn <1163240699@qq.com>
2025-07-15 12:59:16 +0100

    reslove conflicts

--- docs/src/tests/unit_tests/LogoPage.test.tsx
+++ docs/src/tests/unit_tests/LogoPage.test.tsx
@@ -8,7 +8,7 @@ import LogoPage from '../../pages/LogoPage';
  * 测试 LogoPage 组件
  */
 describe('LogoPage 组件', () => {
-  it('应当渲染 “Logo Page” 标题', () => {
+  it('渲染 “Logo Page” ', () => {
     render(<LogoPage />);
     // 这里改成查找标题文本
     expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Logo Page');
