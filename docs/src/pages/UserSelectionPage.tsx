d1f72733858c37a59dc3b3a81ab631d5487696d2
Zhiling05 <liuzhilingwh@gmail.com>
2025-07-23 09:11:56 +0100

    update: the style of filter component in gallery; the width of cards in user selection page and guide home

--- docs/src/pages/UserSelectionPage.tsx
+++ docs/src/pages/UserSelectionPage.tsx
@@ -89,10 +89,10 @@ export default function UserSelectionPage() {
                   className="base-card"
                   onClick={() => navigate(role.route)}
                   style={{
-                    padding: '1rem 1.5rem',
-                    width: '50vw',
-                    maxWidth: '600px',
-                    minWidth: '300px'
+                    padding: '1rem 2rem', 
+                    width: '80vw', 
+                    maxWidth: '50rem', 
+                    minWidth: '20rem' 
                   }}
                 >
                   <div className="card-content">
