8f1971a6ae14e7ab9a3ff36b7f5ae23c1efbcf99
Charlyn <1163240699@qq.com>
2025-08-15 21:41:29 +0800

    add tutorial and siderbar test

--- docs/src/tests/unit_tests/roles/optometrist_tests/records/Records.test.tsx
+++ docs/src/tests/unit_tests/roles/optometrist_tests/records/Records.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import Records from '../../../../pages/optometrist/Records';
+import Records from '../../../../../pages/optometrist/Records';
 import { MemoryRouter } from 'react-router-dom';
 
 // Mock Header, Sidebar, BottomNav 组件
